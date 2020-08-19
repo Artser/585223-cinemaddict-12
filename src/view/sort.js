0bbdba2959859cf31a297bde73e1decddc3e43d4
Artser <artserlisa@mail.ru>
2020-08-19 20:32:21 +0300

    111

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,6 +1,5 @@
 import AbstractView from "./abstract.js";
 import {SortType} from "../const.js";
-
 const createSortTemplate = () => {
   return `<ul class="sort">
       <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
@@ -8,30 +7,23 @@ const createSortTemplate = () => {
       <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE_DOWN}">Sort by rating</a></li>
     </ul>`;
 };
-
 export default class Sorting extends AbstractView {
   constructor() {
     super();
-
     this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
   }
-
   getTemplate() {
     return createSortTemplate();
   }
-
   _sortTypeChangeHandler(evt) {
     if (evt.target.tagName !== `A`) {
       return;
     }
-
     evt.preventDefault();
     this._callback.sortTypeChange(evt.target.dataset.sortType);
   }
-
   setSortTypeChangeHandler(callback) {
     this._callback.sortTypeChange = callback;
     this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
-
