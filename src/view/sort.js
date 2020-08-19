bf2431218f87aae7dd53147038d2b99c9eeb0a13
Artser <artserlisa@mail.ru>
2020-08-19 20:28:46 +0300

    111

--- src/view/sort.js
+++ src/view/sort.js
@@ -2,7 +2,7 @@ import AbstractView from "./abstract.js";
 import {SortType} from "../const.js";
 
 const createSortTemplate = () => {
-  return `<ul class="sort1">
+  return `<ul class="sort">
       <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
       <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE_UP}">Sort by date</a></li>
       <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE_DOWN}">Sort by rating</a></li>
